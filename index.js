2a71a8eac684309adc6f6051c8055a395032a556
devanshud13 <devanshubhatnagar09@gmail.com>
2023-07-21 14:02:09 +0530

    todos

---
+++ index.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/week.html")
+})
+app.get("/script.js", function (request, response) {
+    response.sendFile(__dirname + "/script.js")
+})
+app.get("/todos", function (request, response) {
+    const name = request.query.name;
+    getTodos(name,false, function (error, todos) {
+        if (error) {
+            response.status(500);
+            response.json({ error: error });
+        } else {
+            response.status(200);
+            response.json(todos);
+        }
+    });
+})
+// app.put("/", function (request, response) {
+//     const todo = request.body;
+//     savetodos(todo, function (error) {
+//         if (error) {
+//             response.status(500);
+//             response.json({ error: error });
+//         } else {
+//             response.status(200);
+//             response.send();
+//         }
+//     })
+// })
+app.patch("/todos/:id", function (request, response) {
+    const id = parseInt(request.params.id);
+    fs.readFile(__dirname + "/result.json", "utf-8", function (error, data) {
+      if (error) {
+        response.status(500);
+        response.json({ error: error });
+      } else {
+        let todos = JSON.parse(data);
+        const todoToUpdate = todos.find((todo) => todo.id === id);
+        if (todoToUpdate) {
+          todoToUpdate.marked = request.body.marked;
+          fs.writeFile(__dirname + "/result.json", JSON.stringify(todos, null, 2), function (err) {
+            if (err) {
+              response.status(500);
+              response.json({ error: err });
+            } else {
+              response.status(200);
+              response.json(todoToUpdate);
+            }
+          });
+        } else {
+          response.status(404);
+          response.json({ message: "Todo not found." });
+        }
+      }
+    });
+  });
+app.post("/", function (request, response) {
+    // todos.push(request.body);
+    const todo = request.body;
+    savetodos(todo, function (error,savedTodo) {
+        if (error) {
+            response.status(500);
+            response.json({ error: error });
+        } else {
+    response.status(200);
+    response.send(savedTodo);
+        }
+    })
+});
+app.delete("/", function (request, response) {
+    const text = request.body.text;
+    const username = request.body.username;
+    getTodos(null,true, function (error, todos) {
+        if (error) {
+            response.status(500);
+            response.json({ error: error });
+        } else {
+            const filteredTodos = todos.filter(function (todoItem) {
+                return todoItem.text !== text;
+            });
+            fs.writeFile(__dirname + "/result.json", JSON.stringify(filteredTodos, null, 2), function (err) {
+                if (err) {
+                    response.status(500);
+                    response.json({ error: err });
+                } else {
+                    response.status(200);
+                    response.send();
+                }
+            });
+        }
+    });
+});
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
+function getTodos(username,all,callback) {
+    fs.readFile(__dirname + "/result.json","utf-8", function (error, data) {
+            if (error) {
+                callback(error);
+            }else{
+                if(data.length === 0){
+                    data = "[]";
+                }
+                try{
+                    let todos = JSON.parse(data);
+                   if(all){
+                       callback(null,todos);
+                       return;
+                     }
+                    const filteredTodos = todos.filter(function (todo) {
+                        return todo.username === username;
+                    })
+                    callback(null,filteredTodos);
+                }catch(error){
+                    callback(null , []);
+                }
+            }
+    })
+}
+// function savetodos(todo,callback){
+//   getTodos(null,true, function (error, todos) {
+//       if (error) {
+//           callback(error);
+//       } else {
+//           todos.push(todo);
+
+//   fs.writeFile(__dirname + "/result.json", JSON.stringify(todos,null,2), function (err) {
+//       if (err) {
+//           callback(err);
+//       }
+//       callback();
+//   })
+// }
+// });
+// }
+function savetodos(todo, callback) {
+  getTodos(null, true, function (error, todos) {
+      if (error) {
+          callback(error);
+      } else {
+          // Check if the todo already exists in the list
+          const existingTodoIndex = todos.findIndex(
+              (t) => t.username === todo.username && t.text === todo.text
+          );
+
+          if (existingTodoIndex !== -1) {
+              // If the todo already exists, update its properties
+              todos[existingTodoIndex] = todo;
+          } else {
+              // If the todo doesn't exist, add it to the list
+              todos.push(todo);
+          }
+
+          fs.writeFile(__dirname + "/result.json", JSON.stringify(todos, null, 2), function (err) {
+              if (err) {
+                  callback(err);
+              }
+              callback();
+          });
+      }
+  });
+}
---
+++ script.js
@@ -0,0 +1,129 @@
+const userName = prompt("enter your name");
+const prevText = document.getElementById("todo-input");
+let todosFetched = false;
+getTodos();
+function callButton(event) {
+    if (event.key === "Enter") {
+        handleChange();
+    }
+}
+function handleChange() {
+    const inputvalue = prevText.value;
+    addtodo(inputvalue,false);
+    prevText.value = "";
+}
+function addtodo(inputvalue,id,marked) {
+    const numi = new Date()
+    const num = numi.getTime();
+    console.log(num);
+    const newli = document.createElement("div");
+    newli.setAttribute("id", id);
+    if(marked){
+        newli.innerHTML = `<div id = ${num} class ="special">
+<li id=" newList" style="text-decoration: line-through">${inputvalue}</li>
+<div class = "cut">
+<div class="form-check">
+  <input class="form-check-input" checked type="checkbox" onclick = handleCut(${num})  value="" id="flexCheckDefault">
+<button id="todo-delete"onclick = handleDelete(${num}) >
+<i class="fi fi-bs-cross id="fi"></i></button>
+</div>
+</div>`
+    }
+    else{
+        newli.innerHTML = `<div id = ${num} class ="special">
+<li id=" newList">${inputvalue}</li>
+<div class = "cut">
+<div class="form-check">
+  <input class="form-check-input"  type="checkbox" onclick = handleCut(${num})  value="" id="flexCheckDefault">
+<button id="todo-delete"onclick = handleDelete(${num}) >
+<i class="fi fi-bs-cross id="fi"></i></button>
+</div>
+</div>`
+    }
+    document.getElementById("list").appendChild(newli);
+        handleSave(inputvalue, num,marked); 
+}
+function handleCut(id){
+    const div = document.getElementById(id);
+    if(div.style.textDecoration === "line-through"){
+        div.style.textDecoration = "none";
+        handleMarked(id,false);
+    }
+    else{
+        div.style.textDecoration = "line-through";
+        handleMarked(id,true);
+
+    }
+}
+function handleMarked(id,marked){
+    fetch(`/todos/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+            username: userName,
+            id: id,
+            marked: marked,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+}
+function handleDelete(num) {
+    const button = document.getElementById(num);
+    const todo = button.closest(".special"); // Find the closest parent element with class "special"
+    
+    deleteTodo(todo.querySelector("li").innerText, function (error) {
+        if (error) {
+            alert(error);
+        } else {
+            todo.remove();
+        }
+    });
+}
+function handleSave(todo, num,marked= false) {
+    fetch("/", {
+        method: "POST",
+        body: JSON.stringify({
+            username: userName,
+            id: num,
+            text: todo,
+            marked: marked,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+}
+ function getTodos() {
+    fetch("/todos?name=" + userName)
+        .then(function (response) {
+            if (response.status !== 200) {
+                throw new Error("Something went wrong");
+            }
+            return response.json();
+        })
+        .then(function (todos) {
+            todos.forEach(function (todo) {
+                const marked = todo.marked === true;
+                addtodo(todo.text, todo.id,todo.marked);
+            });
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+}
+  function deleteTodo(todo, callback) {
+    fetch("/", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({text: todo, username: userName, }),
+    }).then(function (response) {
+        if (response.status === 200) {
+          callback();
+        } else {
+          callback("Something went wrong");
+        }
+      });
+}  
